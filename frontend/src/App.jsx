2029a7624dae7e7b9b5e9ca73c1e2eb6a23ae3a8
Kamil Michal <kamil3893@gmail.com>
2025-06-17 22:40:17 +0000

    routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,20 @@ import { createClient } from '@supabase/supabase-js'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
 
+import Home from './pages/home'
+import Login from './pages/login'
+  
 const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  import.meta.env.VITE_SUPABASE_ANOsN_KEY
 )
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   )
 }
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -2,6 +2,7 @@
 import { useState } from 'react'
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Link } from 'react-router-dom'
 
 const navigation = [
   { name: 'About Us', href: '#' },
@@ -38,9 +39,9 @@ export default function Home() {
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            <Link to="/login" className="text-sm/6 font-semibold text-gray-900">
               Log in <span aria-hidden="true">&rarr;</span>
-            </a>
+            </Link>
           </div>
         </nav>
         <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
