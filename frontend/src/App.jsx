67d0f766299e296ff32e92b9d5ea62a3d3c30535
Kamil Michal <kamil3893@gmail.com>
2025-06-17 02:19:42 +0000

    stuff

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,33 +3,15 @@ import { useState, useEffect } from 'react'
 import { createClient } from '@supabase/supabase-js'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import TailwindTest from './TailwindTest'
 
 const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY
 )
 
-  export default function App() {
-    const [session, setSession] = useState(null)
-
-    useEffect(() => {
-      supabase.auth.getSession().then(({ data: { session } }) => {
-        setSession(session)
-      })
-
-      const {
-        data: { subscription },
-      } = supabase.auth.onAuthStateChange((_event, session) => {
-        setSession(session)
-      })
-
-      return () => subscription.unsubscribe()
-    }, [])
-
-    if (!session) {
-      return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
-    }
-    else {
-      return (<div>Logged in!</div>)
-    }
-  }
+export default function App() {
+  return <TailwindTest />
+}
